bb89f9c8c73a806c64ae88d4917a8b1131eeeb74
root <root@bogon.intra.legendsec.com>
2017-02-08 12:55:34 +0100

    webpack搭建

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const config = {
+    entry: {
+        'practice1': path.join(__dirname , '/src/practice1/index')
+    },
+    output:{
+        path:  path.join(__dirname , 'src'),
+        filename: "[name]/bundle.js"
+        // chunkFilename设置
+    },
+    module: {
+        loaders: [
+        //转化es6语法
+            {
+                test: /\.(js|es6)$/,
+                loader: 'babel',
+                exclude: /node_modules/
+            }
+        ],
+
+
+    },
+    plugins: {
+
+    }
+}
+
+module.exports = config;
