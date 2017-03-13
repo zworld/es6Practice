ebb337a58a11fdd8d59821a39a587f5b7b0c07bb
zhongxinzhi <1123858432@qq.com>
2017-03-13 11:47:22 +0800

    补充array转换方法

--- src/practice5/bundle.js
+++ src/practice5/bundle.js
@@ -50,6 +50,14 @@
 
 	var _getIterator3 = _interopRequireDefault(_getIterator2);
 
+	var _from = __webpack_require__(106);
+
+	var _from2 = _interopRequireDefault(_from);
+
+	var _slice = __webpack_require__(114);
+
+	var _slice2 = _interopRequireDefault(_slice);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	// import "babel-polyfill"
@@ -59,154 +67,177 @@
 
 	//ES5
 	{
-	    //监测是否是数组 1、Object.prototype.toString.call() 2、Array.isArray()
-	    var arr = [1, 2, 3, 4, 5, 6];
-	    console.log(Object.prototype.toString.call(arr) == '[object Array]', Array.isArray(arr));
-	}
-	{
-	    //转换方法
-	    var _arr = [1, 2, 3, 4];
-	    console.log(_arr.toString()); //数组转成字符串用逗号隔开
-	    console.log(_arr.valueOf()); //返回数组
-	    console.log(_arr.join('!')); //返回！分割的字符串
-	}
-	{
-	    //栈方法 push pop 会改变原本数组
-	    var _arr2 = new Array();
-	    _arr2.push('testPush1');
-	    _arr2.push('testPush2', 'testPush3');
-	    console.log(_arr2);
-	    _arr2.pop();
-	    console.log(_arr2);
-	}
-	{
-	    //队列方法 shift unshift 会改变原本数组
-	    var _arr3 = new Array();
-	    _arr3.unshift('testShift1');
-	    _arr3.unshift('testShift2');
-	    _arr3.unshift('testShift3');
-	    console.log(_arr3);
-	    _arr3.shift();
-	    console.log(_arr3);
-	}
-	{
-	    //重排序方法 reverse sort 会改变原本数组
-	    var _arr4 = [2, 1, 5, 7, 3, 4];
-	    _arr4.reverse();
-	    console.log(_arr4);
-	    var arrCopy1 = _arr4.concat(); //用数组操作方法来实现数组的深拷贝
-	    var arrCopy2 = _arr4.concat(); //用数组操作方法来实现数组的深拷贝
-	    arrCopy1.sort();
-	    console.log(arrCopy1);
+	    {
+	        //监测是否是数组 1、Object.prototype.toString.call() 2、Array.isArray()
+	        var arr = [1, 2, 3, 4, 5, 6];
+	        console.log(Object.prototype.toString.call(arr) == '[object Array]', Array.isArray(arr));
+	    }
+	    {
+	        //转换方法
+	        var _arr = [1, 2, 3, 4];
+	        console.log(_arr.toString()); //数组转成字符串用逗号隔开
+	        console.log(_arr.valueOf()); //返回数组
+	        console.log(_arr.join('!')); //返回！分割的字符串
+	    }
+	    {
+	        //栈方法 push pop 会改变原本数组
+	        var _arr2 = new Array();
+	        _arr2.push('testPush1');
+	        _arr2.push('testPush2', 'testPush3');
+	        console.log(_arr2);
+	        _arr2.pop();
+	        console.log(_arr2);
+	    }
+	    {
+	        //队列方法 shift unshift 会改变原本数组
+	        var _arr3 = new Array();
+	        _arr3.unshift('testShift1');
+	        _arr3.unshift('testShift2');
+	        _arr3.unshift('testShift3');
+	        console.log(_arr3);
+	        _arr3.shift();
+	        console.log(_arr3);
+	    }
+	    {
+	        //重排序方法 reverse sort 会改变原本数组
+	        var _arr4 = [2, 1, 5, 7, 3, 4];
+	        _arr4.reverse();
+	        console.log(_arr4);
+	        var arrCopy1 = _arr4.concat(); //用数组操作方法来实现数组的深拷贝
+	        var arrCopy2 = _arr4.concat(); //用数组操作方法来实现数组的深拷贝
+	        arrCopy1.sort();
+	        console.log(arrCopy1);
 
-	    //sort如果第一个参数小于第二个返回负数   sort不传参，默认调用tostring,然后比较字符串
-	    arrCopy2.sort(function (item1, item2) {
-	        return item1 - item2;
-	    });
-	    console.log(arrCopy2);
-	}
-	{
-	    //操作方法splice slice concat 注意splice会改变原来的数组
-	    var _arr5 = [1, 2, 3, 4, 5];
-	    console.log(_arr5.concat(6, 7), _arr5.concat([6, 7]), _arr5.concat([6, [7, 8]])); //一层数组会被遍历
-	    console.log(_arr5.slice(2, 3)); //第二个参数是截取的位置
-	    _arr5.splice(2, 1); //第二个参数是个数,返回截取的数组
-	    console.log(_arr5);
-	    _arr5.splice(2, 0, 3, 3.1); //splice第三个及之后的参数是插入的数据
-	    console.log(_arr5);
-	}
-	{
-	    ///位置方法 indexof lastindexOf 第二个参数都是查找的起始点
-	    var _arr6 = [1, 2, 3, 4, 5, 6];
-	    console.log(_arr6.indexOf(2, 4));
-	    console.log(_arr6.lastIndexOf(4, 4));
-	}
-	{
-	    //迭代方法 every filter forEach map some 参数均是（item,index,array）
+	        //sort如果第一个参数小于第二个返回负数   sort不传参，默认调用tostring,然后比较字符串
+	        arrCopy2.sort(function (item1, item2) {
+	            return item1 - item2;
+	        });
+	        console.log(arrCopy2);
+	    }
+	    {
+	        //操作方法splice slice concat 注意splice会改变原来的数组
+	        var _arr5 = [1, 2, 3, 4, 5];
+	        console.log(_arr5.concat(6, 7), _arr5.concat([6, 7]), _arr5.concat([6, [7, 8]])); //一层数组会被遍历
+	        console.log(_arr5.slice(2, 3)); //第二个参数是截取的位置
+	        _arr5.splice(2, 1); //第二个参数是个数,返回截取的数组
+	        console.log(_arr5);
+	        _arr5.splice(2, 0, 3, 3.1); //splice第三个及之后的参数是插入的数据
+	        console.log(_arr5);
+	    }
+	    {
+	        ///位置方法 indexof lastindexOf 第二个参数都是查找的起始点
+	        var _arr6 = [1, 2, 3, 4, 5, 6];
+	        console.log(_arr6.indexOf(2, 4));
+	        console.log(_arr6.lastIndexOf(4, 4));
+	    }
+	    {
+	        //迭代方法 every filter forEach map some 参数均是（item,index,array）
 
-	    var _arr7 = [1, 2, 3, 4, 5, 6];
-	    //every,遍历每一项 如果均返回true，则结果返回true,如果某一项没返回true,终止递归，返回false
-	    var everyResult = _arr7.every(function (item, index, array) {
-	        return true;
-	    });
-	    console.log(everyResult);
+	        var _arr7 = [1, 2, 3, 4, 5, 6];
+	        //every,遍历每一项 如果均返回true，则结果返回true,如果某一项没返回true,终止递归，返回false
+	        var everyResult = _arr7.every(function (item, index, array) {
+	            return true;
+	        });
+	        console.log(everyResult);
 
-	    //filter 运行每一项 返回结果为true的项组成数组
-	    var filterResult = _arr7.filter(function (item, index, arr) {
-	        if (item < 4) return true;
-	    });
-	    console.log(filterResult);
+	        //filter 运行每一项 返回结果为true的项组成数组
+	        var filterResult = _arr7.filter(function (item, index, arr) {
+	            if (item < 4) return true;
+	        });
+	        console.log(filterResult);
 
-	    //forEach遍历每一项，无返回值
-	    var forEachResult = _arr7.forEach(function (item, index, array) {});
+	        //forEach遍历每一项，无返回值
+	        var forEachResult = _arr7.forEach(function (item, index, array) {});
 
-	    //map 返回每次函数调用的结果组成的数组,没有返回则返回数组该项为undefined
-	    var mapResult = _arr7.map(function (item, index, array) {
-	        return Math.pow(item, 2);
-	    });
-	    console.log(mapResult);
+	        //map 返回每次函数调用的结果组成的数组,没有返回则返回数组该项为undefined
+	        var mapResult = _arr7.map(function (item, index, array) {
+	            return Math.pow(item, 2);
+	        });
+	        console.log(mapResult);
 
-	    //some 迭代每一项，如果有一项返回true 则返回true
+	        //some 迭代每一项，如果有一项返回true 则返回true
 
-	    var someResult = _arr7.some(function (item, index, array) {
-	        if (item == 1) return true;
-	    });
-	    console.log(someResult);
-	}
-	{
-	    //归并方法 reduce reduceRight
-	    var _arr8 = [1, 2, 3, 4, 5, 6, 7];
-	    //reduce 第一次执行 prev是数组第一项
-	    var sum = _arr8.reduce(function (prev, cur, index, array) {
-	        return prev + cur;
-	    });
-	    console.log(sum);
+	        var someResult = _arr7.some(function (item, index, array) {
+	            if (item == 1) return true;
+	        });
+	        console.log(someResult);
+	    }
+	    {
+	        //归并方法 reduce reduceRight
+	        var _arr8 = [1, 2, 3, 4, 5, 6, 7];
+	        //reduce 第一次执行 prev是数组第一项
+	        var sum = _arr8.reduce(function (prev, cur, index, array) {
+	            return prev + cur;
+	        });
+	        console.log(sum);
+	    }
 	}
 
 	//ES6
 	{
-	    //数组实例的find()和findIndex()查找到第一个返回为true的值终止递归，find返回数值，findindx返回位置
-	    var _arr9 = [5, 6, 7, 8, 9, 10];
-	    var result = _arr9.find(function (value, index, arr) {
-	        return value > 7;
-	    });
-	    console.log(result);
-	    result = _arr9.findIndex(function (value, index, arr) {
-	        return value > 7;
-	    });
-	    console.log(result);
-	}
-	{
-	    //数组实例的entries()，keys()和values(),数组支持for....of,用作遍历
-	    var _arr10 = [5, 6, 7, 8, 9];
+	    {
+	        //Array.from()将类数组，domlist,arguments,set等转成数组
+	        //ES5中用Array.slice.call()来转换，同时domlist可以用[...domllist]来转换
+	        (function fun() {
+	            var arg = arguments;
+	            console.log(Object.prototype.toString.call(arg));
+	            // arg = Array.from(arguments);
+	            // arg = [...arg]
+	            arg = _slice2.default.call(null, arg);
+	            console.log(Object.prototype.toString.call(arg));
 
-	    var _iteratorNormalCompletion = true;
-	    var _didIteratorError = false;
-	    var _iteratorError = undefined;
-
-	    try {
-	        for (var _iterator = (0, _getIterator3.default)(_arr10.keys()), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-	            var index = _step.value;
-
-	            console.log(index);
-	        }
-	    } catch (err) {
-	        _didIteratorError = true;
-	        _iteratorError = err;
-	    } finally {
-	        try {
-	            if (!_iteratorNormalCompletion && _iterator.return) {
-	                _iterator.return();
-	            }
-	        } finally {
-	            if (_didIteratorError) {
-	                throw _iteratorError;
-	            }
-	        }
+	            var likeArr = {
+	                0: 1,
+	                length: 1
+	            };
+	            likeArr = (0, _from2.default)(likeArr);
+	            console.log(Object.prototype.toString.call(likeArr));
+	        })(1, 2, 3);
 	    }
+	    {
+	        //数组实例的find()和findIndex()查找到第一个返回为true的值终止递归，find返回数值，findindx返回位置
+	        var _arr9 = [5, 6, 7, 8, 9, 10];
+	        var result = _arr9.find(function (value, index, arr) {
+	            return value > 7;
+	        });
+	        console.log(result);
+	        result = _arr9.findIndex(function (value, index, arr) {
+	            return value > 7;
+	        });
+	        console.log(result);
+	    }
+	    {
+	        //数组实例的entries()，keys()和values(),数组支持for....of,用作遍历
+	        var _arr10 = [5, 6, 7, 8, 9];
 
-	    [1, 2, 3].includes(3, 3);
-	    console.log(_arr10.keys());
+	        var _iteratorNormalCompletion = true;
+	        var _didIteratorError = false;
+	        var _iteratorError = undefined;
+
+	        try {
+	            for (var _iterator = (0, _getIterator3.default)(_arr10.keys()), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+	                var index = _step.value;
+
+	                console.log(index);
+	            }
+	        } catch (err) {
+	            _didIteratorError = true;
+	            _iteratorError = err;
+	        } finally {
+	            try {
+	                if (!_iteratorNormalCompletion && _iterator.return) {
+	                    _iterator.return();
+	                }
+	            } finally {
+	                if (_didIteratorError) {
+	                    throw _iteratorError;
+	                }
+	            }
+	        }
+
+	        [1, 2, 3].includes(3, 3);
+	        console.log(_arr10.keys());
+	    }
 	}
 
 /***/ },
@@ -1055,6 +1086,227 @@
 	    || it['@@iterator']
 	    || Iterators[classof(it)];
 	};
+
+/***/ },
+/* 58 */,
+/* 59 */,
+/* 60 */,
+/* 61 */,
+/* 62 */,
+/* 63 */,
+/* 64 */,
+/* 65 */,
+/* 66 */,
+/* 67 */,
+/* 68 */,
+/* 69 */,
+/* 70 */,
+/* 71 */,
+/* 72 */,
+/* 73 */,
+/* 74 */,
+/* 75 */,
+/* 76 */,
+/* 77 */,
+/* 78 */,
+/* 79 */,
+/* 80 */,
+/* 81 */,
+/* 82 */,
+/* 83 */,
+/* 84 */,
+/* 85 */,
+/* 86 */,
+/* 87 */,
+/* 88 */,
+/* 89 */,
+/* 90 */,
+/* 91 */,
+/* 92 */,
+/* 93 */,
+/* 94 */,
+/* 95 */,
+/* 96 */,
+/* 97 */,
+/* 98 */,
+/* 99 */,
+/* 100 */,
+/* 101 */,
+/* 102 */,
+/* 103 */,
+/* 104 */,
+/* 105 */,
+/* 106 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = { "default": __webpack_require__(107), __esModule: true };
+
+/***/ },
+/* 107 */
+/***/ function(module, exports, __webpack_require__) {
+
+	__webpack_require__(50);
+	__webpack_require__(108);
+	module.exports = __webpack_require__(17).Array.from;
+
+/***/ },
+/* 108 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	var ctx            = __webpack_require__(18)
+	  , $export        = __webpack_require__(15)
+	  , toObject       = __webpack_require__(49)
+	  , call           = __webpack_require__(109)
+	  , isArrayIter    = __webpack_require__(110)
+	  , toLength       = __webpack_require__(38)
+	  , createProperty = __webpack_require__(111)
+	  , getIterFn      = __webpack_require__(57);
+
+	$export($export.S + $export.F * !__webpack_require__(112)(function(iter){ Array.from(iter); }), 'Array', {
+	  // 22.1.2.1 Array.from(arrayLike, mapfn = undefined, thisArg = undefined)
+	  from: function from(arrayLike/*, mapfn = undefined, thisArg = undefined*/){
+	    var O       = toObject(arrayLike)
+	      , C       = typeof this == 'function' ? this : Array
+	      , aLen    = arguments.length
+	      , mapfn   = aLen > 1 ? arguments[1] : undefined
+	      , mapping = mapfn !== undefined
+	      , index   = 0
+	      , iterFn  = getIterFn(O)
+	      , length, result, step, iterator;
+	    if(mapping)mapfn = ctx(mapfn, aLen > 2 ? arguments[2] : undefined, 2);
+	    // if object isn't iterable or it's array with default iterator - use simple case
+	    if(iterFn != undefined && !(C == Array && isArrayIter(iterFn))){
+	      for(iterator = iterFn.call(O), result = new C; !(step = iterator.next()).done; index++){
+	        createProperty(result, index, mapping ? call(iterator, mapfn, [step.value, index], true) : step.value);
+	      }
+	    } else {
+	      length = toLength(O.length);
+	      for(result = new C(length); length > index; index++){
+	        createProperty(result, index, mapping ? mapfn(O[index], index) : O[index]);
+	      }
+	    }
+	    result.length = index;
+	    return result;
+	  }
+	});
+
+
+/***/ },
+/* 109 */
+/***/ function(module, exports, __webpack_require__) {
+
+	// call something on iterator step with safe closing on error
+	var anObject = __webpack_require__(22);
+	module.exports = function(iterator, fn, value, entries){
+	  try {
+	    return entries ? fn(anObject(value)[0], value[1]) : fn(value);
+	  // 7.4.6 IteratorClose(iterator, completion)
+	  } catch(e){
+	    var ret = iterator['return'];
+	    if(ret !== undefined)anObject(ret.call(iterator));
+	    throw e;
+	  }
+	};
+
+/***/ },
+/* 110 */
+/***/ function(module, exports, __webpack_require__) {
+
+	// check on default Array iterator
+	var Iterators  = __webpack_require__(8)
+	  , ITERATOR   = __webpack_require__(47)('iterator')
+	  , ArrayProto = Array.prototype;
+
+	module.exports = function(it){
+	  return it !== undefined && (Iterators.Array === it || ArrayProto[ITERATOR] === it);
+	};
+
+/***/ },
+/* 111 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	var $defineProperty = __webpack_require__(21)
+	  , createDesc      = __webpack_require__(29);
+
+	module.exports = function(object, index, value){
+	  if(index in object)$defineProperty.f(object, index, createDesc(0, value));
+	  else object[index] = value;
+	};
+
+/***/ },
+/* 112 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var ITERATOR     = __webpack_require__(47)('iterator')
+	  , SAFE_CLOSING = false;
+
+	try {
+	  var riter = [7][ITERATOR]();
+	  riter['return'] = function(){ SAFE_CLOSING = true; };
+	  Array.from(riter, function(){ throw 2; });
+	} catch(e){ /* empty */ }
+
+	module.exports = function(exec, skipClosing){
+	  if(!skipClosing && !SAFE_CLOSING)return false;
+	  var safe = false;
+	  try {
+	    var arr  = [7]
+	      , iter = arr[ITERATOR]();
+	    iter.next = function(){ return {done: safe = true}; };
+	    arr[ITERATOR] = function(){ return iter; };
+	    exec(arr);
+	  } catch(e){ /* empty */ }
+	  return safe;
+	};
+
+/***/ },
+/* 113 */,
+/* 114 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = { "default": __webpack_require__(115), __esModule: true };
+
+/***/ },
+/* 115 */
+/***/ function(module, exports, __webpack_require__) {
+
+	__webpack_require__(116);
+	module.exports = __webpack_require__(17).Array.slice;
+
+/***/ },
+/* 116 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	var $export    = __webpack_require__(15)
+	  , html       = __webpack_require__(45)
+	  , cof        = __webpack_require__(11)
+	  , toIndex    = __webpack_require__(40)
+	  , toLength   = __webpack_require__(38)
+	  , arraySlice = [].slice;
+
+	// fallback for not array-like ES3 strings and DOM objects
+	$export($export.P + $export.F * __webpack_require__(26)(function(){
+	  if(html)arraySlice.call(html);
+	}), 'Array', {
+	  slice: function slice(begin, end){
+	    var len   = toLength(this.length)
+	      , klass = cof(this);
+	    end = end === undefined ? len : end;
+	    if(klass == 'Array')return arraySlice.call(this, begin, end);
+	    var start  = toIndex(begin, len)
+	      , upTo   = toIndex(end, len)
+	      , size   = toLength(upTo - start)
+	      , cloned = Array(size)
+	      , i      = 0;
+	    for(; i < size; i++)cloned[i] = klass == 'String'
+	      ? this.charAt(start + i)
+	      : this[start + i];
+	    return cloned;
+	  }
+	});
 
 /***/ }
 /******/ ]);
--- src/practice5/index.es6
+++ src/practice5/index.es6
@@ -5,132 +5,157 @@
 
 //ES5
 {
-    //监测是否是数组 1、Object.prototype.toString.call() 2、Array.isArray()
-    let arr = [1,2,3,4,5,6];
-    console.log(Object.prototype.toString.call(arr) == '[object Array]',Array.isArray(arr))
-}
-{
-    //转换方法
-    let arr = [1,2,3,4];
-    console.log(arr.toString())//数组转成字符串用逗号隔开
-    console.log(arr.valueOf())//返回数组
-    console.log(arr.join('!'))//返回！分割的字符串
-}
-{
-    //栈方法 push pop 会改变原本数组
-    let arr = new Array()
-    arr.push('testPush1')
-    arr.push('testPush2','testPush3')
-    console.log(arr);
-    arr.pop();
-    console.log(arr)
-}
-{
-    //队列方法 shift unshift 会改变原本数组
-    let arr = new Array();
-    arr.unshift('testShift1');
-    arr.unshift('testShift2');
-    arr.unshift('testShift3')
-    console.log(arr);
-    arr.shift()
-    console.log(arr)
-}
-{
-    //重排序方法 reverse sort 会改变原本数组
-    let arr = [2,1,5,7,3,4];
-    arr.reverse();
-    console.log(arr)
-    let arrCopy1 = arr.concat();//用数组操作方法来实现数组的深拷贝
-    let arrCopy2 = arr.concat();//用数组操作方法来实现数组的深拷贝
-    arrCopy1.sort()
-    console.log(arrCopy1)
+    {
+        //监测是否是数组 1、Object.prototype.toString.call() 2、Array.isArray()
+        let arr = [1,2,3,4,5,6];
+        console.log(Object.prototype.toString.call(arr) == '[object Array]',Array.isArray(arr))
+    }
+    {
+        //转换方法
+        let arr = [1,2,3,4];
+        console.log(arr.toString())//数组转成字符串用逗号隔开
+        console.log(arr.valueOf())//返回数组
+        console.log(arr.join('!'))//返回！分割的字符串
+    }
+    {
+        //栈方法 push pop 会改变原本数组
+        let arr = new Array()
+        arr.push('testPush1')
+        arr.push('testPush2','testPush3')
+        console.log(arr);
+        arr.pop();
+        console.log(arr)
+    }
+    {
+        //队列方法 shift unshift 会改变原本数组
+        let arr = new Array();
+        arr.unshift('testShift1');
+        arr.unshift('testShift2');
+        arr.unshift('testShift3')
+        console.log(arr);
+        arr.shift()
+        console.log(arr)
+    }
+    {
+        //重排序方法 reverse sort 会改变原本数组
+        let arr = [2,1,5,7,3,4];
+        arr.reverse();
+        console.log(arr)
+        let arrCopy1 = arr.concat();//用数组操作方法来实现数组的深拷贝
+        let arrCopy2 = arr.concat();//用数组操作方法来实现数组的深拷贝
+        arrCopy1.sort()
+        console.log(arrCopy1)
 
-    //sort如果第一个参数小于第二个返回负数   sort不传参，默认调用tostring,然后比较字符串
-    arrCopy2.sort(function (item1,item2) {
-        return item1 - item2
-    })
-    console.log(arrCopy2)
-}
-{
-    //操作方法splice slice concat 注意splice会改变原来的数组
-    let arr = [1,2,3,4,5];
-    console.log(arr.concat(6,7),arr.concat([6,7]),arr.concat([6,[7,8]]))//一层数组会被遍历
-    console.log(arr.slice(2,3))//第二个参数是截取的位置
-    arr.splice(2,1)//第二个参数是个数,返回截取的数组
-    console.log(arr)
-    arr.splice(2,0,3,3.1)//splice第三个及之后的参数是插入的数据
-    console.log(arr)
-}
-{
-    ///位置方法 indexof lastindexOf 第二个参数都是查找的起始点
-    let arr = [1,2,3,4,5,6];
-    console.log(arr.indexOf(2,4))
-    console.log(arr.lastIndexOf(4,4))
-}
-{
-    //迭代方法 every filter forEach map some 参数均是（item,index,array）
+        //sort如果第一个参数小于第二个返回负数   sort不传参，默认调用tostring,然后比较字符串
+        arrCopy2.sort(function (item1,item2) {
+            return item1 - item2
+        })
+        console.log(arrCopy2)
+    }
+    {
+        //操作方法splice slice concat 注意splice会改变原来的数组
+        let arr = [1,2,3,4,5];
+        console.log(arr.concat(6,7),arr.concat([6,7]),arr.concat([6,[7,8]]))//一层数组会被遍历
+        console.log(arr.slice(2,3))//第二个参数是截取的位置
+        arr.splice(2,1)//第二个参数是个数,返回截取的数组
+        console.log(arr)
+        arr.splice(2,0,3,3.1)//splice第三个及之后的参数是插入的数据
+        console.log(arr)
+    }
+    {
+        ///位置方法 indexof lastindexOf 第二个参数都是查找的起始点
+        let arr = [1,2,3,4,5,6];
+        console.log(arr.indexOf(2,4))
+        console.log(arr.lastIndexOf(4,4))
+    }
+    {
+        //迭代方法 every filter forEach map some 参数均是（item,index,array）
 
-    let arr = [1,2,3,4,5,6];
-    //every,遍历每一项 如果均返回true，则结果返回true,如果某一项没返回true,终止递归，返回false
-    let everyResult = arr.every(function (item,index,array) {
-        return true
-    })
-    console.log(everyResult)
+        let arr = [1,2,3,4,5,6];
+        //every,遍历每一项 如果均返回true，则结果返回true,如果某一项没返回true,终止递归，返回false
+        let everyResult = arr.every(function (item,index,array) {
+            return true
+        })
+        console.log(everyResult)
 
-    //filter 运行每一项 返回结果为true的项组成数组
-    let filterResult = arr.filter(function (item,index,arr) {
-        if(item < 4 ) return true
-    })
-    console.log(filterResult)
-    
-    //forEach遍历每一项，无返回值
-    let forEachResult = arr.forEach(function (item,index,array) {
-    })
+        //filter 运行每一项 返回结果为true的项组成数组
+        let filterResult = arr.filter(function (item,index,arr) {
+            if(item < 4 ) return true
+        })
+        console.log(filterResult)
 
-    //map 返回每次函数调用的结果组成的数组,没有返回则返回数组该项为undefined
-    var mapResult = arr.map(function (item,index,array) {
-        return Math.pow(item,2)
-    })
-    console.log(mapResult)
+        //forEach遍历每一项，无返回值
+        let forEachResult = arr.forEach(function (item,index,array) {
+        })
 
-    //some 迭代每一项，如果有一项返回true 则返回true
+        //map 返回每次函数调用的结果组成的数组,没有返回则返回数组该项为undefined
+        var mapResult = arr.map(function (item,index,array) {
+            return Math.pow(item,2)
+        })
+        console.log(mapResult)
 
-    let someResult = arr.some(function (item,index,array) {
-        if(item == 1) return true
-    })
-    console.log(someResult)
+        //some 迭代每一项，如果有一项返回true 则返回true
 
-}
-{
-    //归并方法 reduce reduceRight
-    let arr = [1,2,3,4,5,6,7];
-    //reduce 第一次执行 prev是数组第一项
-    let sum = arr.reduce(function (prev,cur,index,array) {
-        return prev + cur;
-    })
-    console.log(sum)
+        let someResult = arr.some(function (item,index,array) {
+            if(item == 1) return true
+        })
+        console.log(someResult)
+
+    }
+    {
+        //归并方法 reduce reduceRight
+        let arr = [1,2,3,4,5,6,7];
+        //reduce 第一次执行 prev是数组第一项
+        let sum = arr.reduce(function (prev,cur,index,array) {
+            return prev + cur;
+        })
+        console.log(sum)
+    }
+
 }
 
 //ES6
 {
-    //数组实例的find()和findIndex()查找到第一个返回为true的值终止递归，find返回数值，findindx返回位置
-    let arr = [5,6,7,8,9,10];
-    let result = arr.find(function(value,index,arr){
-        return value > 7
-    })
-    console.log(result)
-    result = arr.findIndex(function (value,index,arr) {
-        return value > 7　
-    })
-    console.log(result)
-}
-{
-    //数组实例的entries()，keys()和values(),数组支持for....of,用作遍历
-    let arr = [ 5,6,7,8,9 ];
+    {
+        //Array.from()将类数组，domlist,arguments,set等转成数组
+        //ES5中用Array.slice.call()来转换，同时domlist可以用[...domllist]来转换
+        (function fun(){
+            let arg = arguments;
+           console.log(Object.prototype.toString.call(arg));
+            // arg = Array.from(arguments);
+            // arg = [...arg]
+            arg = Array.slice.call(null,arg);
+            console.log(Object.prototype.toString.call(arg))
 
-    for(let index of arr.keys()){
-        console.log(index)
+            let likeArr = {
+                0: 1,
+                length: 1
+            }
+            likeArr = Array.from(likeArr);
+            console.log(Object.prototype.toString.call(likeArr));
+        })(1,2,3)
     }
-    [1, 2, 3].includes(3, 3);
-    console.log(arr.keys())
+    {
+        //数组实例的find()和findIndex()查找到第一个返回为true的值终止递归，find返回数值，findindx返回位置
+        let arr = [5,6,7,8,9,10];
+        let result = arr.find(function(value,index,arr){
+            return value > 7
+        })
+        console.log(result)
+        result = arr.findIndex(function (value,index,arr) {
+            return value > 7
+        })
+        console.log(result)
+    }
+    {
+        //数组实例的entries()，keys()和values(),数组支持for....of,用作遍历
+        let arr = [ 5,6,7,8,9 ];
+
+        for(let index of arr.keys()){
+            console.log(index)
+        }
+        [1, 2, 3].includes(3, 3);
+        console.log(arr.keys())
+    }
 }
+
